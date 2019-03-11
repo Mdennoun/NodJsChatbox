cb752640f16319fec897e25fdf4bfa2a16379ca3
Mohamed <mohamedennoun@gmail.com>
2019-03-11 15:02:38 +0100

    correct heroku

--- server.js
+++ server.js
@@ -1,23 +1,14 @@
-
-var http = require('http');
-
-const hostname = "https://ancient-ridge-88345.herokuapp.com"
-
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Word');
-});
-
 const express = require('express')
 const app = express()
 
 app.get('/hello', function (req, res) {
   res.send('Hello World!')
 })
-var port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Example app listening on port ' = port )
+  console.log('Example app listening on port ' + port)
 })
+
 
 /*
 
