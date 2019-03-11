06f49216f186560162c2831e479a0f712103878f
Mohamed <mohamedennoun@gmail.com>
2019-03-11 15:30:46 +0100

    chatbox

--- server.js
+++ server.js
@@ -3,11 +3,26 @@ const app = express()
 
 app.use(express.json());
 
-app.get('/hello', function (req, res) {
+app.get('/hello', function (req, res) 
+{
   res.send('Hello World!')
 })
+
+app.post('/chat', function (req, res)
+{
+	if(req.body.msg === 'ville') {
+		res.send("Nous somme à Paris");
+	}
+	if(req.body.msg === 'météo') {
+		res.send("Il fait beau");
+	}
+})
+
+
+
 const port = process.env.PORT || 3000;
-app.listen(port, function () {
+app.listen(port, function () 
+{
   console.log('Example app listening on port ' + port)
 })
 
