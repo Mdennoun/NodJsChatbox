af65fa4b7311922f971bf3292d554369cc7e39a7
Mohamed <mohamedennoun@gmail.com>
2019-03-11 12:35:52 +0100

    Ajout server JS

---
+++ server.js
@@ -0,0 +1,9 @@
+/*
+var http = require('http');
+
+var server = http.createServer(function('/hello', res) {
+  res.writeHead(200);
+  res.end('Salut tout le monde !');
+});
+server.listen(3000);$:*/
+console.log("Coucou");
