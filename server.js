37fc6723f3c5233613383e3a82601a5f61b6ce3b
Mohamed <mohamedennoun@gmail.com>
2019-03-11 14:52:40 +0100

    correct heroku

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.get('/hello', function (req, res) {
   res.send('Hello World!')
 })
 var port = process.env.PORT || 3000
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log('Example app listening on port ' = port )
 })
 
 /*
