32398984906278a9463813e48a8bf2917a9b3286
Mohamed <mohamedennoun@gmail.com>
2019-03-11 14:43:10 +0100

    Addes heroku

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 
 var http = require('http');
 
+const hostname = "https://ancient-ridge-88345.herokuapp.com"
+
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end('Hello Word !');
+  res.end('Hello Word');
 });
-server.listen(3000);
+
+const express = require('express')
+const app = express()
+
+app.get('hello/', function (req, res) {
+  res.send('Hello World!')
+})
+var port = process.env.PORT || 3000
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
+
 /*
 
-console.log("Hello Word");
+console.log(portport, hostname, );
 */
